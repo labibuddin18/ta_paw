6d69f212f4fa4b3501a38a98c36443bf8d2129e4
labibuddin18 <tokomanfaat6@gmail.com>
2025-11-21 10:43:31 +0700

    db

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 $host='localhost';
 $user='root';
 $password='';
-$db='ppd';
+$db='ppdb';
 try {
     $pdo=new PDO("mysql:host=$host;dbname=$db",$user,$password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
